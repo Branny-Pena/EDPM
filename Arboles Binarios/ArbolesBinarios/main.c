ec0a712934d6edca946960fd5d2c99c6b787a0d7
Branny <pena.branny@pucp.edu.pe>
2022-09-17 09:55:02 -0500

    Clase 17/9 ArbolesBinarios

---
+++ Arboles Binarios/ArbolesBinarios/main.c
@@ -0,0 +1,16 @@
+/* 
+ * File:   main.c
+ * Author: Branny
+ *
+ * Created on September 17, 2022, 9:44 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "ArbolesBinarios.h"
+
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+

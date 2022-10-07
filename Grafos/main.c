5e9c97974a2bc6571a389c5ff9ba069dfbf375ff
Branny <pena.branny@pucp.edu.pe>
2022-10-07 07:13:07 -0500

    .?

---
+++ Grafos/main.c
@@ -0,0 +1,15 @@
+/* 
+ * File:   main.c
+ * Author: Branny
+ *
+ * Created on October 4, 2022, 8:57 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
